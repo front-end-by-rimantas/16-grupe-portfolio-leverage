2dca3e3afe499f33a3908103e43896964082754d
Vytautas <66961507+wycka0214@users.noreply.github.com>
2020-08-21 22:43:57 +0300

    komment

--- js/renderBestServices.js
+++ js/renderBestServices.js
@@ -7,12 +7,12 @@ function renderBestServices( selector, data ) {
     for ( let i=0; i<size; i++ ) {
         const service = data[i];
         console.log(service);
-        
+
         HTML += `<div class="card">
-                    <i class="icon fa fa-database" aria-hidden="true"></i>
-                    <h3>Digital Marketing</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-                    <a class="arrow " href="#"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></i></a>
+                    ${service.icon}
+                    <h3>${service.title}</h3>
+                    <p>${service.descripcion}</p>
+                    ${service.link}
                 </div>`;
     }
 
