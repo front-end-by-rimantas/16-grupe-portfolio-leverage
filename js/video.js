c17df7d2add8d1f9b53a23130952071043d08ba8
Aliux02 <aliux02@gmail.com>
2020-08-25 21:36:11 +0300

    main HTML structure in JS module done

--- js/video.js
+++ js/video.js
@@ -5,49 +5,36 @@ class Video {
     constructor(param) {
         this.selector = param.selector;
         this.DOM = null;
-        /*this.init();*/
+        
         this.renderVideo();
        
     }
-    /*init() {
-        if (!this.isValidSelector()) {
-            return;
-        }
-        this.renderVideo();
-    }
-
-    /*isValidSelector() {
-        if (typeof this.selector !== 'string') {
-            console.warn('Selector should be a "string" type.');
-            return false;
-        }
-        if (this.selector === '') {
-            console.warn('Selector should not be an empty string.');
-            return false;
-        }
-
-        this.DOM = document.querySelector(this.selector);
-        if (!this.DOM) {
-            console.warn('Could not find any element by given selector.');
-            return false;
-        }
-
-        return true;
-    }*/
-    
-    renderVideo (){
+ renderVideo (){
         this.DOM = document.querySelector(this.selector)
         this.DOM.innerHTML = ` <div class="row">
                                     <div  class="col-12">
                                         <h2>${videoData.title}</h2>
                                         <p>${videoData.text}</p>  
                                     </div>
-                                    <div class="col-12 videobox">
+                                    <div class="col-12 picturebox">
                                         <a href=${videoData.link}> 
                                             <i class="fa fa-play"></i>
                                             <img src="../img/watchVideoPic.jpg" alt="videoPhoto">
                                         </a>
                                     </div>
+                                </div>
+                                <div class="video-background">
+                                        <div class="video-header">header
+                                            <i class="fa fa-clone"></i>
+                                            <i class="fa fa-search-minus"></i>
+                                            <i class="fa fa-search-plus"></i>
+                                            <i class="fa fa-arrows-alt"></i>
+                                            <i class="fa fa-times"></i>
+                                        </div>
+                                        <div class="video-position">
+                                            <iframe src="//www.youtube.com/embed/7e90gBu4pas?wmode=opaque&amp;autoplay=1&amp;enablejsapi=1" frameborder="0"></iframe>
+                                        </div>  
+                                        <div class="video-footer">footer</div>
                                 </div>`;
                                 
     }
