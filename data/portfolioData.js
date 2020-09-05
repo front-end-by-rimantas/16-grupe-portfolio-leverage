ef9c4efcc99c29ac04d74f346c9e46bb2100afc0
Aliux <aliux02@gmail.com>
2020-09-05 16:09:46 +0300

    added photo, data file created

---
+++ data/portfolioData.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        img: 'news1.jpg',
+        title: 'Laiminga',
+        tags: ['tech', 'digital','photography'],
+    },
+    {
+        img: 'news2.jpg',
+        title: 'VRSet',
+        tags: ['tech', 'branding'],
+
+    },
+    {
+        img: 'news3.jpg',
+        title: 'Laptopas',
+        tags: ['tech', 'digital'],
+    },
+    {
+        img: 'news4.jpg',
+        title: 'Serveris',
+        tags: ['design', 'digital', 'photography'],
+    },
+    {
+        img: 'news5.jpg',
+        title: 'Susikaupus',
+        tags: ['design', 'branding'],
+    },
+    {
+        img: 'news6.jpg.jpg',
+        title: 'Projektas',
+        tags: ['design', 'branding', 'tech'],
+    },
+];
