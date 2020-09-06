5210ec6e49209ed50c05ce6a590f7b18e7cd049b
Vytautas <66961507+wycka0214@users.noreply.github.com>
2020-09-06 14:31:09 +0300

    class add

--- js/renderSkills.js
+++ js/renderSkills.js
@@ -4,7 +4,10 @@ function renderSkills(selector, data) {
     const size = data.length;
     for (let i = 0; i < size; i++) {
         HTML += `<div class="bar col-12 col-sm-12 col-md-6 col-lg-3">
-                   SKILS 1234
+                            <div class="skill-gauge-value">
+                                <h2 class="value">${0}</h2>
+                                <span>%</span>
+                            </div>
                 </div>`;
     }
     return DOM.innerHTML = HTML;
