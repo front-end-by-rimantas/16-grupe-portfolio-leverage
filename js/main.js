955022781f5842136315422d93a6c3f2141cc8f8
Vytautas <66961507+wycka0214@users.noreply.github.com>
2020-09-06 17:49:53 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@
 import { renderBestServices } from './renderBestServices.js';
 import data from '../js/data.js';
 import {Video} from './video.js';
+import { Header } from './header.js';
+
 
 const { bestServices } = data;
 renderBestServices('#best-services .service', bestServices );
 
 new Video ({selector: '#video'});
+new Header ({selector: '#header'});
 
 
 
