5f35b8f85cf9e869f040ed3844bc6e1432e5d781
Aliux02 <aliux02@gmail.com>
2020-09-06 12:02:24 +0300

    img deployed

--- js/PortfolioListItem.js
+++ js/PortfolioListItem.js
@@ -17,7 +17,16 @@ class PortfolioListItem {
 
     }
     render(){
-        const HTML = `<div class="item">${this.data.img}</div>`
+        const HTML = `<div class="item">
+                        <div class="card">
+                            <img src="./img/portfolio/${this.data.img}" alt="${this.data.title}">
+                            <div clas="hover"></div>
+                            <div class="cardText">
+                                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                            </div>
+                        </div>    
+                    </div>`
         this.parentDOM.insertAdjacentHTML('beforeend',HTML);
         this.DOM = this.parentDOM.querySelector('.item');
     }
